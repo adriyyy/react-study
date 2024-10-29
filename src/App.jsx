3a6c37a86d62c2df788391e9d050982de0fdf285
Adriana <goiua4493@gmail.com>
2024-10-29 15:39:05 +0200

    Last commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,65 @@ import MainLayout from "./layouts/MainLayout";
 import Homepage from "./pages/Homepage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { loader } from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<Homepage />} />
-      <Route path="/jobs" element={<JobsPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
-);
-function App() {
+const App = () => {
+  // Add new jobs
+  const addJob = async (newJob) => {
+    await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  // Delete job
+  const deleteJob = async (id) => {
+    await fetch(`/api/jobs/${id}`, {
+      method: "DELETE",
+    });
+    return;
+  };
+
+  // Update job
+  const updateJob = async (updatedJob) => {
+    await fetch(`/api/jobs/${updatedJob.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedJob),
+    });
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route
+          path="/jobs/:id"
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={loader}
+        />
+        <Route path="/add-job" element={<AddJobPage addJob={addJob} />} />
+        <Route
+          path="/edit-job/:id"
+          element={<EditJobPage updateJob={updateJob} />}
+          loader={loader}
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
   return <RouterProvider router={router} />;
-}
+};
 
 export default App;
